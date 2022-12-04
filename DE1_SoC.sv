16c2e1f602f6e9639002c5e72d5662702dd92773
katleiahramos <katleiahramos@gmail.com>
2022-12-03 17:27:39 -0800

    remove unused refernce code

--- DE1_SoC.sv
+++ DE1_SoC.sv
@@ -1,36 +1,5 @@
 // Top-level module that defines the I/Os for the DE1 SoC board	
 
-// module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, SW, LEDR, GPIO_1, CLOCK_50);
-// 	output logic [6:0]  HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
-// 	output logic [9:0]  LEDR;
-// 	input  logic [3:0]  KEY;
-// 	input  logic [9:0]  SW;
-// 	output logic [35:0] GPIO_1;
-// 	input logic CLOCK_50;
-	
-// 	logic [31:0] clk;
-// 	logic SYSTEM_CLOCK;
-// 	// Set up system base clock to 1526 Hz (50 MHz / 2**(14+1))
-// 	assign SYSTEM_CLOCK = clk[14]; // increase freq for no-flicker, lower for brighter
-// 	logic [15:0][15:0]RedPixels; // 16 x 16 array representing red LEDs
-// 	logic [15:0][15:0]GrnPixels; // 16 x 16 array representing green LEDs
-// 	logic RST;                   // reset - toggle this on startup
-		
-// 	assign RST = ~KEY[3];
-
-// 	clock_divider divider (.clock(CLOCK_50), .divided_clocks(clk));
-// 	LEDDriver Driver (.CLK(SYSTEM_CLOCK), .RST, .EnableCount(1'b1), .RedPixels, .GrnPixels, .GPIO_1);
-	
-// 	LED_test test (.RST, .RedPixels, .GrnPixels);
-
-// 	assign HEX0 = '1;
-// 	assign HEX1 = '1;
-// 	assign HEX2 = '1;
-// 	assign HEX3 = '1;
-// 	assign HEX4 = '1;
-// 	assign HEX5 = '1;
-// endmodule
-
 module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW, GPIO_1);
 	input logic CLOCK_50; // 50MHz clock.
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
